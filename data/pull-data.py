75a16d2b053f011037b4e89fa2842953e63c0557
Andreas <andreas@maanex.me>
2023-06-14 09:45:22 +0000

    no more linter >:(

--- data/pull-data.py
+++ data/pull-data.py
@@ -25,7 +25,6 @@ BIKE_URL_BASE = 'https://raw.githubusercontent.com/od-ms/radverkehr-zaehlstellen
 
 
 
-
 def clear_folder(path, match = '*'):
   [ os.remove(f) for f in glob.glob(os.path.join(path, match)) ]
 
