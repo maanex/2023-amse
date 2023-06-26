30b472f09eb2dc328e2242833c236d413492919f
Andreas <andreas@maanex.me>
2023-06-26 21:57:06 +0000

    continue progress

--- data/pull-data.py
+++ data/pull-data.py
@@ -17,9 +17,9 @@ OUT_DATA_FOLDER = './out/'
 WEATHER_URLS = [
   ('cloudiness', 'produkt_n_stunde_19891001_20221231_01766.txt', 'https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/hourly/cloudiness/historical/stundenwerte_N_01766_19891001_20221231_hist.zip'),
   ('extreme_wind', 'produkt_fx_stunde_19900701_20221231_01766.txt', 'https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/hourly/extreme_wind/historical/stundenwerte_FX_01766_19900701_20221231_hist.zip'),
-  ('moisture', 'produkt_tf_stunde_19891001_20221231_01766.txt', 'https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/hourly/moisture/historical/stundenwerte_TF_01766_19891001_20221231_hist.zip'),
   ('temperature', 'produkt_tu_stunde_19891001_20221231_01766.txt', 'https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/hourly/air_temperature/historical/stundenwerte_TU_01766_19891001_20221231_hist.zip'),
   ('visibility', 'produkt_vv_stunde_19891001_20221231_01766.txt', 'https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/hourly/visibility/historical/stundenwerte_VV_01766_19891001_20221231_hist.zip'),
+  ('precipitation', 'produkt_rr_stunde_19950901_20221231_01766.txt', 'https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/hourly/precipitation/historical/stundenwerte_RR_01766_19950901_20221231_hist.zip'),
   ('wind', 'produkt_ff_stunde_19820101_20221231_01766.txt', 'https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/hourly/wind/historical/stundenwerte_FF_01766_19820101_20221231_hist.zip'),
 ]
 
@@ -109,68 +109,46 @@ def download_and_process_bike(month, year):
 
   return out
 
-def merge_weather_data(cloudiness, extreme_wind, moisture, temperature, visibility, wind):
+def merge_weather_data(cloudiness, extreme_wind, temperature, visibility, precipitation, wind):
   vals = {}
 
   for date, v1 in cloudiness:
     if not date in vals: vals[date] = {}
-    vals[date]['clouds_qn8'] = v1
-  for date, v1, v2 in extreme_wind:
+    vals[date]['clouds_vn'] = v1
+  for date, v1 in extreme_wind:
     if not date in vals: vals[date] = {}
-    vals[date]['ewind_qn8'] = v1
-    vals[date]['ewind_fx911'] = v2
-  for date, v1, v2, v3, v4, v5, v6, v7, v8 in moisture:
+    vals[date]['ewind_fx911'] = v1
+  for date, v1, v2 in temperature:
     if not date in vals: vals[date] = {}
-    vals[date]['moist_qn8'] = v1
-    vals[date]['moist_absf_std'] = v2
-    vals[date]['moist_vp_std'] = v3
-    vals[date]['moist_tf_std'] = v4
-    vals[date]['moist_p_std'] = v5
-    vals[date]['moist_tt_std'] = v6
-    vals[date]['moist_rf_std'] = v7
-    vals[date]['moist_td_std'] = v8
-  for date, v1, v2, v3 in temperature:
+    vals[date]['temp_tt_tu'] = v1
+    vals[date]['temp_rf_tu'] = v2
+  for date, v1 in visibility:
     if not date in vals: vals[date] = {}
-    vals[date]['temp_qn9'] = v1
-    vals[date]['temp_tt_tu'] = v2
-    vals[date]['temp_rf_tu'] = v3
-  for date, v1, v2 in visibility:
+    vals[date]['vis_v_vv'] = v1
+  for date, v1 in precipitation:
     if not date in vals: vals[date] = {}
-    vals[date]['vis_qn8'] = v1
-    vals[date]['vis_v_vv'] = v2
-  for date, v1, v2, v3 in wind:
+    vals[date]['prec_r1'] = v1
+  for date, v1, v2 in wind:
     if not date in vals: vals[date] = {}
-    vals[date]['wind_qn3'] = v1
-    vals[date]['wind_f'] = v2
-    vals[date]['wind_d'] = v3
+    vals[date]['wind_f'] = v1
+    vals[date]['wind_d'] = v2
 
-  out = [ 'date;clouds_qn8;ewind_qn8;ewind_fx911;moist_qn8;moist_absf_std;moist_vp_std;moist_tf_std;moist_p_std;moist_tt_std;moist_rf_std;moist_td_std;temp_qn9;temp_tt_tu;temp_rf_tu;vis_qn8;vis_v_vv;wind_qn3;wind_f;wind_d' ]
+  out = [ 'date;clouds_vn;ewind_fx911;temp_tt_tu;temp_rf_tu;vis_v_vv;prec_r1;wind_f;wind_d' ]
   for k in vals:
     v = vals[k]
     out.append(';'.join(map(str, [
       k,
-      v.get('clouds_qn8', 0),
-      v.get('ewind_qn8', 0),
+      v.get('clouds_vn', 0),
       v.get('ewind_fx911', 0),
-      v.get('moist_qn8', 0),
-      v.get('moist_absf_std', 0),
-      v.get('moist_vp_std', 0),
-      v.get('moist_tf_std', 0),
-      v.get('moist_p_std', 0),
-      v.get('moist_tt_std', 0),
-      v.get('moist_rf_std', 0),
-      v.get('moist_td_std', 0),
-      v.get('temp_qn9', 0),
       v.get('temp_tt_tu', 0),
       v.get('temp_rf_tu', 0),
-      v.get('vis_qn8', 0),
       v.get('vis_v_vv', 0),
-      v.get('wind_qn3', 0),
+      v.get('prec_r1', 0),
       v.get('wind_f', 0),
       v.get('wind_d', 0)
     ])))
 
-  return '\n'.join(out[0:10])
+  return '\n'.join(out)
 
 #
 
@@ -180,14 +158,14 @@ def main(skipdownload=False):
     download_weather_urls()
     clear_folder(RAW_DATA_FOLDER, '*.zip')
 
-  cloudiness    = parsefile_weather('cloudiness',   lambda a: a[1].startswith('20'), lambda a: (int(a[1]), int(a[2])))
-  extreme_wind  = parsefile_weather('extreme_wind', lambda a: a[1].startswith('20'), lambda a: (int(a[1]), int(a[2]), float(a[3])))
-  moisture      = parsefile_weather('moisture',     lambda a: a[1].startswith('20'), lambda a: (int(a[1]), int(a[2]), float(a[3]), float(a[4]), float(a[5]), float(a[6]),  float(a[7]), float(a[8]), float(a[9])))
-  temperature   = parsefile_weather('temperature',  lambda a: a[1].startswith('20'), lambda a: (int(a[1]), int(a[1]), float(a[3]), float(a[4])))
-  visibility    = parsefile_weather('visibility',   lambda a: a[1].startswith('20'), lambda a: (int(a[1]), int(a[2]), float(a[4])))
-  wind          = parsefile_weather('wind',         lambda a: a[1].startswith('20'), lambda a: (int(a[1]), int(a[2]), float(a[3]), int(a[4])))
+  cloudiness    = parsefile_weather('cloudiness',    lambda a: a[1].startswith('202'), lambda a: (int(a[1]), int(a[4])))
+  extreme_wind  = parsefile_weather('extreme_wind',  lambda a: a[1].startswith('202'), lambda a: (int(a[1]), float(a[3])))
+  temperature   = parsefile_weather('temperature',   lambda a: a[1].startswith('202'), lambda a: (int(a[1]), float(a[3]), float(a[4])))
+  visibility    = parsefile_weather('visibility',    lambda a: a[1].startswith('202'), lambda a: (int(a[1]), float(a[4])))
+  precipitation = parsefile_weather('precipitation', lambda a: a[1].startswith('202'), lambda a: (int(a[1]), float(a[3])))
+  wind          = parsefile_weather('wind',          lambda a: a[1].startswith('202'), lambda a: (int(a[1]), float(a[3]), int(a[4])))
 
-  weather_merged = merge_weather_data(cloudiness, extreme_wind, moisture, temperature, visibility, wind)
+  weather_merged = merge_weather_data(cloudiness, extreme_wind, temperature, visibility, precipitation, wind)
 
   with open('raw/weather_merged.csv', 'w') as out:
     out.write(weather_merged)
