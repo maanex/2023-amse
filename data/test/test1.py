5260cf07949661f7f4ac1b9942d5ba6cc98f80f5
Andreas <andreas@maanex.me>
2023-06-28 11:06:13 +0000

    test

--- data/test/test1.py
+++ data/test/test1.py
@@ -1,11 +1,17 @@
 import pytest
-from os import path
+from os import path, getcwd
 
 def test_files_exist():
-  assert path.exists('./raw/cloudiness.csv')
-  assert path.exists('./raw/extreme_wind.csv')
-  assert path.exists('./raw/temperature.csv')
-  assert path.exists('./raw/visibility.csv')
-  assert path.exists('./raw/precipitation.csv')
-  assert path.exists('./raw/wind.csv')
-  assert path.exists('./raw/biketemp.csv')
+  print(getcwd())
+  print(getcwd())
+  print(getcwd())
+  print(getcwd())
+  print(getcwd())
+  print(getcwd())
+  assert path.exists('../raw/cloudiness.csv')
+  assert path.exists('../raw/extreme_wind.csv')
+  assert path.exists('../raw/temperature.csv')
+  assert path.exists('../raw/visibility.csv')
+  assert path.exists('../raw/precipitation.csv')
+  assert path.exists('../raw/wind.csv')
+  assert path.exists('../raw/biketemp.csv')
