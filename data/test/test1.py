aa2d8a00aece2f1a6ba06a8829ca4fa50e130789
Andreas <andreas@maanex.me>
2023-06-28 11:04:25 +0000

    update tests

--- data/test/test1.py
+++ data/test/test1.py
@@ -2,10 +2,10 @@ import pytest
 from os import path
 
 def test_files_exist():
-  assert path.exists('./data/raw/cloudiness.csv')
-  assert path.exists('./data/raw/extreme_wind.csv')
-  assert path.exists('./data/raw/temperature.csv')
-  assert path.exists('./data/raw/visibility.csv')
-  assert path.exists('./data/raw/precipitation.csv')
-  assert path.exists('./data/raw/wind.csv')
-  assert path.exists('./data/raw/biketemp.csv')
+  assert path.exists('./raw/cloudiness.csv')
+  assert path.exists('./raw/extreme_wind.csv')
+  assert path.exists('./raw/temperature.csv')
+  assert path.exists('./raw/visibility.csv')
+  assert path.exists('./raw/precipitation.csv')
+  assert path.exists('./raw/wind.csv')
+  assert path.exists('./raw/biketemp.csv')
