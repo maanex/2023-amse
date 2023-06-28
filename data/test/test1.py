e4fc6bbb17268d1a907034e7e518eeeed3668221
Andreas <andreas@maanex.me>
2023-06-28 09:01:41 +0000

    update test

--- data/test/test1.py
+++ data/test/test1.py
@@ -2,10 +2,10 @@ import pytest
 from os import path
 
 def test_files_exist():
-  assert path.exists('../raw/bikedata.json')
+  assert path.exists('../raw/biketemp.csv')
   assert path.exists('../raw/cloudiness.csv')
   assert path.exists('../raw/extreme_wind.csv')
-  assert path.exists('../raw/moisture.csv')
   assert path.exists('../raw/temperature.csv')
   assert path.exists('../raw/visibility.csv')
+  assert path.exists('../raw/precipitation.csv')
   assert path.exists('../raw/wind.csv')
